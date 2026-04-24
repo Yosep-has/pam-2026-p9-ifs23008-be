d36c6819445f8da2303ddd3922e95cb83f7dfb82
Yosep simatupang <yosepmsimatupang678@gmail.com>
2026-04-24 10:05:01 +0700

    first push flutter ai

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import os
 from app import create_app
+from app.config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(debug=True, port=Config.APP_PORT)
